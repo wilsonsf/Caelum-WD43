cd9cbb23c581608de08ea6cdc290a254abcb267b
Wilson <email@email.com>
2015-07-22 13:00:09 -0300

    8.7-2 Serapando cabecalho em um arquivo a parte

--- index.php
+++ index.php
@@ -9,21 +9,6 @@
   <link rel="stylesheet" href="css/mobile.css" media="(max-width: 939px)">
 </head>
 <body>
-  <header class="container">
-    <h1><a href="index.html"><img src="img/logo.png" alt="Mirror Fashion"></a></h1>
-    <div class="sacola">
-      Nenhum item na sacola de compras
-    </div>
-    <nav class="menu-opcoes">
-      <ul>
-        <li><a href="#">Sua Conta</a></li>
-        <li><a href="#">Lista de Desejos</a></li>
-        <li><a href="#">Cartão Fidelidade</a></li>
-        <li><a href="sobre.html">Sobre</a></li>
-        <li><a href="#">Ajuda</a></li>
-      </ul>
-    </nav>
-  </header>
   <div class="container destaque">
     <section class="busca">
       <h2>Busca</h2>
