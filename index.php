3acfefb348aef39fc261051efe7067290868a765
Wilson <email@email.com>
2015-07-22 12:47:03 -0300

    8.7 PHP-Include: Página produto e index php criados.

---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Mirror Fashion</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/estilos.css">
+  <link rel="stylesheet" href="css/mobile.css" media="(max-width: 939px)">
+</head>
+<body>
+  <header class="container">
+    <h1><a href="index.html"><img src="img/logo.png" alt="Mirror Fashion"></a></h1>
+    <div class="sacola">
+      Nenhum item na sacola de compras
+    </div>
+    <nav class="menu-opcoes">
+      <ul>
+        <li><a href="#">Sua Conta</a></li>
+        <li><a href="#">Lista de Desejos</a></li>
+        <li><a href="#">Cartão Fidelidade</a></li>
+        <li><a href="sobre.html">Sobre</a></li>
+        <li><a href="#">Ajuda</a></li>
+      </ul>
+    </nav>
+  </header>
+  <div class="container destaque">
+    <section class="busca">
+      <h2>Busca</h2>
+      
+      <form id="form-busca" action="http://www.google.com.br/search">
+        <input type="search" id="q" name="q">
+        <input type="image" src="img/busca.png">
+      </form>
+    </section> <!-- fim .busca -->
+    <section class="menu-departamentos">
+      <h2>Departamentos</h2>
+      
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Blusas e Camisas</a>
+            <ul>
+              <li><a href="#">Manga curta</a></li>
+              <li><a href="#">Manga comprida</a></li>
+              <li><a href="#">Camisa social</a></li>
+              <li><a href="#">Camisa casual</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Calças</a></li>
+          <li><a href="#">Saias</a></li>
+          <li><a href="#">Vestidos</a></li>
+          <li><a href="#">Sapatos</a></li>
+          <li><a href="#">Bolsas e Carteiras</a></li>
+          <li><a href="#">Acessórios</a></li>
+        </ul>
+      </nav>
+    </section> <!-- fim .menu-departamentos -->
+    
+    <img src="img/destaque-home.png" alt="Promoção: Big City Night">
+    <a href="#" class="pause"></a>
+  </div> <!-- fim .container .destaque -->
+  
+  <div class="container paineis">
+    <section class="painel novidades">
+      <h2>Novidades</h2>
+      <ol>
+        <li>
+          <a href="produto.html">
+            <figure>
+              <img src="img/produtos/miniatura1.png" alt="">
+              <figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="produto.html">
+            <figure>
+              <img src="img/produtos/miniatura2.png" alt="">
+              <figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="produto.html">
+            <figure>
+              <img src="img/produtos/miniatura3.png" alt="">
+              <figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+            </figure>
+        </li>
+        <li>
+          <a href="produto.html">
+            <figure>
+              <img src="img/produtos/miniatura4.png" alt="">
+              <figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="produto.html">
+            <figure>
+              <img src="img/produtos/miniatura5.png" alt="">
+              <figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="produto.html">
+            <figure>
+              <img src="img/produtos/miniatura6.png" alt="">
+              <figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+            </figure>
+          </a>
+        </li>
+      </ol>
+    </section> <!-- fim .painel .novidades -->
+
+    <section class="painel mais-vendidos">
+      <h2>Mais Vendidos</h2>
+      <ol>
+        <li>
+          <a href="produto.html">
+            <figure>
+              <img src="img/produtos/miniatura1.png" alt="">
+              <figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="produto.html">
+            <figure>
+              <img src="img/produtos/miniatura2.png" alt="">
+              <figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="produto.html">
+            <figure>
+              <img src="img/produtos/miniatura3.png" alt="">
+              <figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="produto.html">
+            <figure>
+              <img src="img/produtos/miniatura4.png" alt="">
+              <figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="produto.html">
+            <figure>
+              <img src="img/produtos/miniatura5.png" alt="">
+              <figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a href="produto.html">
+            <figure>
+              <img src="img/produtos/miniatura6.png" alt="">
+              <figcaption>Fuzz Cardigan por R$ 129,90</figcaption>
+            </figure>
+          </a>
+        </li>
+      </ol>
+    </section> <!-- fim .painel .mais-vendidos -->
+  </div> <!-- fim .container .paineis -->
+  <footer>
+    <div class="container">
+      <img src="img/logo-rodape.png" alt="Logo Mirror Fashion">
+      
+      <ul class="social">
+        <li><a href="http://facebook.com/mirrorfashion">Facebook</a></li>
+        <li><a href="http://twitter.com/mirrorfashion">Twitter</a></li>
+        <li><a href="htto://plus.google.com/mirrorfashion">Google+</a></li>
+      </ul> <!-- fim .social -->
+    </div> <!-- fim .container -->
+  </footer>
+  <script src="js/home.js"></script>
+</body>
+</html>
---
+++ produto.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Mirror Fashion</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/estilos.css">
+  <link rel="stylesheet" href="css/mobile.css" media="(max-width: 939px)">
+</head>
+<body>
+  
+</body>
+</html>
