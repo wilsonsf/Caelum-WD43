b15e2b999082578562457e9d90bf226b8228a4bf
Wilson <email@email.com>
2015-07-23 11:19:36 -0300

    9.14: Fundo - adicionado fundo e estilização da página produto

--- produto.php
+++ produto.php
@@ -3,67 +3,69 @@
   $cabecalho_css = '<link rel="stylesheet" href="css/produto.css">';
   include_once("cabecalho.php");
 ?>
-  <div class="container">
-    <div class="produto">
-      <h1>Fuzzy Cardigan</h1>
-      <p>por apenas R$129,00</p>
-      
-      <form>
-        <fieldset class="cores">
-          <legend>Escolha a cor:</legend>
-          <input type="radio" name="cor" value="verde" id="verde" checked>
-          <label for="verde">
-            <img src="img/produtos/foto2-verde.png" alt="verde">
-          </label>
-          
-          <input type="radio" name="cor" value="rosa" id="rosa">
-          <label for="rosa">
-            <img src="img/produtos/foto2-rosa.png" alt="rosa">
-          </label>
-          
-          <input type="radio" name="cor" value="azul" id="azul">
-          <label for="azul">
-            <img src="img/produtos/foto2-azul.png" alt="azul">
-          </label>
-        </fieldset> <!-- fim .cores -->
-        <fieldset class="tamanhos">
-          <legend>Escolha o tamanho:</legend>
-          
-          <input type="range" min="36" max="46" value="42" step="2" name="tamanho" id="tamanho">
-        </fieldset><!-- fim .tamanhos -->
-        <input type="submit" class="comprar" value="Comprar">
-        
-      </form>
-    </div> <!-- fim .produto -->
-    <div class="detalhes">
-      <h2>Detalhes do Produto</h2>
-      <p>Esse é o melhor casaco de Cardigã que você já viu. Excelente material italiano com estampa desenhada pelos artesãos da comunidade Krotor nas ilhas gregas. Compre já e receba hoje mesmo pela nossa entrega a jato.</p>
-      <table>
-        <thead>
-          <tr>
-            <th>Características</th>
-            <th>Detalhe</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Modelo</td>
-            <td>Cardigã 7845</td>
-          </tr>
-          <tr>
-            <td>Material</td>
-            <td>Algodão e poliester</td>
-          </tr>
-          <tr>
-            <td>Cores</td>
-            <td>Azul, Rosa e Verde</td>
-          </tr>
-          <tr>
-            <td>Lavagem</td>
-            <td>Lavar a mão</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div> <!-- fim .container -->
+  <div class="produto-back">
+     <div class="container">
+       <div class="produto">
+         <h1>Fuzzy Cardigan</h1>
+         <p>por apenas R$129,00</p>
+         
+         <form>
+           <fieldset class="cores">
+             <legend>Escolha a cor:</legend>
+             <input type="radio" name="cor" value="verde" id="verde" checked>
+             <label for="verde">
+               <img src="img/produtos/foto2-verde.png" alt="verde">
+             </label>
+             
+             <input type="radio" name="cor" value="rosa" id="rosa">
+             <label for="rosa">
+               <img src="img/produtos/foto2-rosa.png" alt="rosa">
+             </label>
+             
+             <input type="radio" name="cor" value="azul" id="azul">
+             <label for="azul">
+               <img src="img/produtos/foto2-azul.png" alt="azul">
+             </label>
+           </fieldset> <!-- fim .cores -->
+           <fieldset class="tamanhos">
+             <legend>Escolha o tamanho:</legend>
+             
+             <input type="range" min="36" max="46" value="42" step="2" name="tamanho" id="tamanho">
+           </fieldset><!-- fim .tamanhos -->
+           <input type="submit" class="comprar" value="Comprar">
+           
+         </form>
+       </div> <!-- fim .produto -->
+       <div class="detalhes">
+         <h2>Detalhes do Produto</h2>
+         <p>Esse é o melhor casaco de Cardigã que você já viu. Excelente material italiano com estampa desenhada pelos artesãos da comunidade Krotor nas ilhas gregas. Compre já e receba hoje mesmo pela nossa entrega a jato.</p>
+         <table>
+           <thead>
+             <tr>
+               <th>Características</th>
+               <th>Detalhe</th>
+             </tr>
+           </thead>
+           <tbody>
+             <tr>
+               <td>Modelo</td>
+               <td>Cardigã 7845</td>
+             </tr>
+             <tr>
+               <td>Material</td>
+               <td>Algodão e poliester</td>
+             </tr>
+             <tr>
+               <td>Cores</td>
+               <td>Azul, Rosa e Verde</td>
+             </tr>
+             <tr>
+               <td>Lavagem</td>
+               <td>Lavar a mão</td>
+             </tr>
+           </tbody>
+         </table>
+       </div>
+     </div>
+   </div> <!-- fim .container -->
 <?php include_once("rodape.php"); ?>
