399239c95d093561605bac213e76e4a6f66a270c
Wilson <email@email.com>
2015-07-23 15:30:13 -0300

    11.5-1,2 Página de Checkout - Adicionado Bootstrap e cabeçalho das páginas

--- checkout.php
+++ checkout.php
@@ -2,12 +2,17 @@
 <html>
 <head>
   <meta charset="UTF-8">
+  <link rel="stylesheet" href="css/bootstrap.css">
   <title>Checkout Mirror Fashion</title>
   <meta name="viewport" content="width=device-width">
 </head>
 <body>
-  <h1>Ótima escolha!</h1>
-  <p>Obrigado por comprar na Mirror Fashion! Preencha seus dados para efetivar a compra</p>
+  <div class="jumbotron">
+    <div class="container">
+      <h1>Ótima escolha!</h1>
+      <p>Obrigado por comprar na Mirror Fashion! Preencha seus dados para efetivar a compra</p>
+    </div> <!-- fim .container -->
+  </div> <!-- fim .jumbotron -->
   <h2>Sua compra</h2>
   <dl>
     <dt>Produto</dt>
